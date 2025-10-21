002ac7b37870db08215b6b63fa1d82ea814d896d
mike-debastiani <mike.debastiani@stud.hslu.ch>
2025-10-21 18:17:50 +0200

    tailwindTest deleted

--- Tailwind_Test/script.js
+++
@@ -1,7 +0,0 @@
-// Web Project - JavaScript
-// Add your JavaScript code here
-
-document.addEventListener('DOMContentLoaded', function() {
-    // Your initialization code here
-    console.log('Web Project loaded');
-});
